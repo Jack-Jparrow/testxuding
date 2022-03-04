31b6c9605f4116f0382a604f61f5c9f30e969df7
杰克杰派罗 <captain-jparrow@qq.com>
2022-03-04 18:31:44 +0800

    modified:   1.js

--- 1.js
+++ 1.js
@@ -1,5 +1,14 @@
+/*
+ * @Author: Jack Jparrow
+ * @Date: 2022-03-04 17:43:27
+ * @LastEditTime: 2022-03-04 18:31:25
+ * @LastEditors: Jack Jparrow
+ * @Description: 
+ */
 var a = 1;
 var b = 2;
+var c = 3;
 console.log("hello world");
 console.log("a = ", a);
 console.log("b = ", b);
+console.log("c = ", c);
